2a5af08f584e53fac01fd3447e363119add50a15
Ruslan Mir <rmir212121@gmail.com>
2022-10-27 21:25:16 +0300

    I/O of a number

--- lab01.asm
+++ lab01.asm
@@ -1,5 +1,6 @@
 
 SECTION .bss
+    size resb 8
 
 SECTION .data
     SizePrompt: db "Enter size of array", 10
@@ -12,6 +13,19 @@ SECTION .text
     global _start ; entry point??
 
     _start:
+    ; input vector size
+    call _getSize 
+    
+    ; input vector
+    ;call _getVector
+
+    ; comlete task
+    ;call _calculate 
+
+    ; output result
+    call _printRes 
+
+
     mov rax, 1 ; syswrite
     mov rdi, 1 ; stdout
     mov rsi, SizePrompt ; message
@@ -20,7 +34,24 @@ SECTION .text
 
     
 
+
+
     ; terminate program
     mov eax, 1 ; exit syscall
     mov ebx, 0 ; error code
     int 80h    ; call kernel
+
+_getSize:
+    mov rax, 0 ;sysread
+    mov rdi, 0 ;stdin
+    mov rsi, size ; write into reserved bytes
+    mov rdx, 8 ; size of input
+    syscall
+    ret
+
+_printRes:
+    mov rax, 1 ; syswrite
+    mov rdi, 1 ; stdout
+    mov rsi, size ; message
+    mov rdx, 8 ; message len
+    syscall
