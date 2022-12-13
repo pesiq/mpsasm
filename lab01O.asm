29239299d926067213f9ebb20be671dd98b5ebb3
pesiq <rmir212121@gmal.com>
2022-12-13 18:27:25 +0300

    Added input
    barely tested

--- lab01O.asm
+++ lab01O.asm
@@ -2,11 +2,14 @@ global _start
 
 SECTION .bss
 
-    vectorData resb: 100
-    resultVectorData resb: 100
+    vectorData: resb 100
+    resultVectorData: resb 100
+
+    pointer: resq 1
     
-    size resq: 1
-    resSize resq: 1
+    number: resq 1
+    size: resq 1
+    resSize: resq 1
 
 SECTION .data
 
@@ -16,7 +19,7 @@ SECTION .data
     vectorString: db "Enter vector elements:", 0xA
     vectorStringSize: equ $-vectorString
 
-    resultString: db "Result vector:".0xA
+    resultString: db "Result vector:", 0xA
     resultStringSize: equ $-resultString
 
 SECTION .text
@@ -28,10 +31,10 @@ _start:
     call _vectorIn
 
 
-    call _solve
+    ;call _solve
 
 
-    call _vectorOut
+    ;call _vectorOut
 
     mov eax, 1 
     mov ebx, 0 
@@ -43,13 +46,47 @@ _start:
 
 _sizein:
 
-    mov eax, 1
-    mov edi, 1
+    mov eax, 0
+    mov edi, 0
     mov esi, size
     mov edx, 8
+    syscall
 
     mov eax, [size]
     sub eax, 0xA30
     mov [size], eax
+
+ret
+
+_vectorIn:
+
+    mov eax, [size]
+
+    mov ebx, vectorData
+
+    
+    loopIn:
+
+    push rax
+    push rbx
+
+    mov eax, 0
+    mov edi, 0
+    mov esi, number
+    mov edx, 8
+    syscall
+
+    mov eax, [number]
+    sub eax, 0xA30
+
+    pop rbx
+    mov [rbx], eax
+    inc ebx
+
+
+    pop rax
+    dec eax
+    cmp eax, 0
+    jg loopIn
 
 ret
